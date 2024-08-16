d794871a0c5b139d3e012e053d9c7379073443b9
Isabelly Dornelas <isabelly.dornelas123@gmail.com>
2024-08-16 14:53:02 -0300

    CRUD da farmacia

--- FARMALUZ/src/App.tsx
+++ FARMALUZ/src/App.tsx
@@ -1,23 +1,47 @@
+import './App.css'
+
+import FormularioCategoria from '../src/components/Categoria/FormularioCategoria/FormularioCategoria'
+import ListaCategoria from '../src/components/Categoria/FormularioCategoria/FormularioCategoria'
+import Footer from '../src/components/Footer/Footer'
+import Navbar from '../src/components/Navbar/NavBar'
+import Home from './Pages/Home/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
-import './App.css'
-import Footer from './components/footer/footer'
-import Home from './components/home/home'
-import Navbar from './components/NavBar/NavBar'
 
+
+
+import { AuthProvider } from './Context/AuthContext';
+import ListaProdutos from '../src/components/Produtos/ListaProdutos/ListaProdutos';
+import FormularioProdutos from '../src/components/Produtos/FormularioProdutos/Formularioprodutos';
+import DeletarProdutos from '../src/components/Produtos/DeletarProdutos/DeletarProdutos';
+
+// No seu componente App
 function App() {
-
-
-  return (
-    <>
-    <BrowserRouter>
-    <Footer/>
-    <Navbar/>
-    <Home/>
-    </BrowserRouter>
-    </>
-  )
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        
+                        
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/Categorias" element={<ListaCategoria />} />
+                        <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+                        <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+                        
+                        <Route path="/produtos" element={<ListaProdutos />} />
+              <Route path="/cadastroprodutos" element={<FormularioProdutos />} />
+              <Route path="/editarprodutos/:id" element={<FormularioProdutos />} />
+              <Route path="/deletarprodutos/:id" element={<DeletarProdutos />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
 
-export default App
+export default App;
--- FARMALUZ/src/components/footer/footer.tsx
+++ FARMALUZ/src/components/footer/footer.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+
 
 function Footer() {
   return (
-    <footer className="bg-blue-500 p-4 mt-8">
-      <div className="container mx-auto text-center text-white">
-        <p>&copy; 2024 Farmácia. Todos os direitos reservados.</p>
+    <>
+      <div className="flex justify-center bg-pink-700 text-white">
+        <div className="container flex flex-col items-center py-4">
+          <p className='text-xl font-bold'>Farmaluzz - um sol para cada manha | Copyright: </p>
+          <p className='text-lg'>Acesse nossas redes sociais</p>
+          <div className='flex gap-2'>
+          </div>
+        </div>
       </div>
-    </footer>
-  );
+    </>
+  )
 }
 
-export default Footer;
+export default Footer
